4b13cfa01f3ec39805afc8bdd7d1764d5ae213d1
hagamenon.oliveira <hagamenon.oliveira@soluti.com.br>
2020-04-16 21:36:27 -0300

    estrutura de pilha simplesmente encadeada

--- src/main/java/com/estruturadados/stack/DinamicStack.java
+++ src/main/java/com/estruturadados/stack/DinamicStack.java
@@ -1,63 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.estruturadados.stack;
 
-/**
- *
- * @author hagan
+/*
+ * @author Hagamenon.Oliveira <hagamenon.oliveira@soluti.com.br>
+ * @Description Estrutura de pilha simplesmene encadeada
  */
 public class DinamicStack {
-    No topo;
+    
+    No top;
     int size;
     
-    public void create(){
-        this.topo = new No();
-        this.topo.create(-1);
+    public DinamicStack() {
+        this.top = new No(-1);
         this.size = 0;
     }
     
-    public void push(int value){
-        No novoNo = new No();
-        novoNo.create(value);
-        novoNo.setNoProximo(topo.getNoProximo());
-        topo.setNoProximo(novoNo);
+    public void push(int valor) {
+        No newNo = new No(valor);
+        newNo.setNoProximo(top.getNoProximo());
+        this.top.setNoProximo(newNo);
         size++;
     }
     
-    public boolean isEmpty(){
-        return topo.getNoProximo() == null;
-    }
-    
-    public int pop(){
-        No NoReturn = topo.getNoProximo();
-        topo.setNoProximo(NoReturn.getNoProximo());
-        size--;
-        return NoReturn.getNoValor();
-    }
-    
-    public No peek(){
-        return topo;
-    }
-
-
-    /**Função responsável por informar o tamamnho atual da pilha*/
-    public int size(){
-        return size;
-    }
-
-    /*printa pilha dinâmica*/
-    public int printAll(No aux){
-        if(aux.getNoProximo() == null){
-            return aux.getNoValor();
-        } else {
-            int response =  printAll(aux.getNoProximo());
-            System.out.println(response);
-            return aux.getNoValor();
+    public int pop() throws Exception{
+        if(this.size == 0) {
+            throw new Exception("pilha vazia");
         }
+        
+        No aux = top.getNoProximo();
+        top.setNoProximo(aux.getNoProximo());
+        
+        return aux.getValor();
     }
-
-
+    
+    public boolean isEmpty() {
+        return this.top.getNoProximo() == null;
+    }
+    
+    public int getSize() {
+        return this.size;
+    }
+    
 }
--- src/main/java/com/estruturadados/stack/No.java
+++ src/main/java/com/estruturadados/stack/No.java
@@ -1,36 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.estruturadados.stack;
 
-/**
- *
- * @author hagan
+/*
+ * @author Hagamenon.Oliveira <hagamenon.oliveira@soluti.com.br>
+ * @Description Estrutura básica que será encadeada
  */
 public class No {
     
+    /*valor que vai receber o nó*/
     int valor;
+    
+    /*Nó a ser encadeado*/
     No proximo;
     
-    void create(int novoValor){
-        this.valor = novoValor;
+    /*construtor do Nó*/
+    public No(int valor){
+        /*ao ser construído não existe nó encadeado*/
+        this.proximo = null;
+        this.valor = valor;
     }
     
-    int getNoValor(){
-        return valor;
+    int getValor() {
+        return this.valor;
     }
     
-    void setNoValor(int novoValor){
-        this.valor = novoValor;
-    }
-    
-    No getNoProximo(){
+    No getNoProximo() {
         return this.proximo;
     }
     
-    void setNoProximo(No newNext){
-        this.proximo = newNext;
+    void setNoProximo(No novoNo) {
+        this.proximo = novoNo;
     }
 }
