e5f98b79844f9903ca9f147c5bea8de6fb35e0d6
hagamenon.oliveira <hagamenon.oliveira@soluti.com.br>
2021-03-08 21:38:01 -0300

    métodos complexos

--- src/main/java/com/estruturadados/ordenacao/MergeSort.java
+++ src/main/java/com/estruturadados/ordenacao/MergeSort.java
@@ -1,71 +1,49 @@
 package com.estruturadados.ordenacao;
 
-import java.util.Arrays;
-
-/**
- *
- * @author Hagamenon.Oliveira<haganicolau@gmail.com>
- */
 public class MergeSort {
-
-    private static int metade1;
-
-    public static void main(String[] args) {
-        int vetor[] = {7,2,9,4,3,8,6,1,5};
-
-        sort(vetor, 0, vetor.length-1);
+    
+    public static void main(String [] args) {
+        int vetor[] = {7,2,9,4,3,0,8,6,1,5};
+        
+        sort(vetor, 0, vetor.length - 1);
         printarVetor(vetor);
     }
-    
-    /**
-     * @description Método recursivo, que faz a quebra do array criando
-     * uma complexidade O(logn)
-     * @author Hagamenon Oliveira <haganicolau@gmail.com>
-     * @return void 
-     */
+
     public static void sort(int vetor[], int inicio, int fim) {
-        if (inicio < fim) {
+        if(inicio < fim) {
             int meio = (inicio + fim) / 2;
             sort(vetor, inicio, meio);
             sort(vetor, meio + 1, fim);
-            intercalar(vetor, inicio, meio, fim);
+            merge(vetor, inicio, meio, fim);
         }
     }
- 
-    /**
-     * @description Método responsável por intercalar e rearranjar os elementos
-     * conforme for voltando no método recursivo
-     * @author Hagamenon Oliveira <haganicolau@gmail.com>
-     * @return void 
-     */
-    public static void intercalar(int vetor[], int inicio, int meio, int fim) {
+
+    public static void merge(int vetor[], int inicio, int meio, int fim) {
 
         int secundario[] = new int[vetor.length];
 
-        /*cópia do vetor*/
-        for (int k = inicio; k <= fim; k++){
+        for(int k = inicio; k <= fim; k++) {
             secundario[k] = vetor[k];
         }
-        
-        int indiceMetade1 = inicio;
-        int indiceMetade2 = meio+1;
-        
-        for (int k = inicio; k <= fim; k++) {
 
-            if(indiceMetade1 > meio) vetor[k] = secundario[indiceMetade2++];
-            else if(indiceMetade2 > fim) vetor[k] = secundario[indiceMetade1++];
-            else if(secundario[indiceMetade1] < secundario[indiceMetade2]) vetor[k] = secundario[indiceMetade1++];
-            else vetor[k] = secundario[indiceMetade2++];
+        int indiceEsquerdo = inicio;
+        int indiceDireito = meio+1;
 
+
+        for(int k = inicio; k <= fim; k++) {
+            if(indiceEsquerdo > meio) vetor[k] = secundario[indiceDireito++];
+            else if(indiceDireito > fim) vetor[k] = secundario[indiceEsquerdo++];
+            else if(secundario[indiceEsquerdo] < secundario[indiceDireito]) vetor[k] = secundario[indiceEsquerdo++];
+            else vetor[k] = secundario[indiceDireito++];
         }
-        
     }
 
     public static void printarVetor(int vetor[]) {
         System.out.print("vetor [ ");
         for(int i = 0; i < vetor.length; i++) {
-            System.out.print(vetor[i]+" ");
+            System.out.print(vetor[i] + " ");
         }
-        System.out.print("];");
+        System.out.print("]; ");
+        System.out.println(" ");
     }
 }
--- src/main/java/com/estruturadados/ordenacao/ShellSort.java
+++ src/main/java/com/estruturadados/ordenacao/ShellSort.java
@@ -1,53 +1,39 @@
 package com.estruturadados.ordenacao;
 
-import java.util.Arrays;
-
-/**
- *
- * @author Hagamenon.Oliveira<haganicolau@gmail.com>
- */
-public class ShellSort {
-
-    public static void main(String [] args) {
-        int vetor[] = {7,2,9,4,3,8,6,1,5};
+public class ShellSort{
+    public static void main(String[] args) {
+        int vetor[] = {7,2,9,4,3,0,8,6,1,5};
         int vetorOrdenado[] = sort(vetor);
-
         printarVetor(vetor);
     }
 
-    /**
-     * @description Método shell sort O(n^2)
-     * @author Hagamenon Oliveira <haganicolau@gmail.com>
-     * @return int [] - vetor ordenado 
-     */
-    public static int[] sort(int[] v) {
-        
-        int h = v.length / 2;
-        int valor, j, i;
-        
-        while(h > 0) {
-            for(i = h; i < v.length; i++) {
-                valor = v[i];
-                j = i;
-                
-                while(j >= h && v[j-h] > valor) {
-                    v[j] = v[j-h];
-                    j = j-h; 
-                }
-                v[j] = valor;
-            }
-          h = h / 2;  
-        }
-        
-       return v;
+    public static int[] sort(int vetor[]) {
+         int h = vetor.length / 2;
+         int key, j, i;
+
+         while(h > 0) {
+             for(i = h; i < vetor.length; i++) {
+                 key = vetor[i];
+                 j = i;
+
+                 while(j >= h && vetor[j - h] > key) {
+                    vetor[j] = vetor[j - h];
+                    j = j - h;
+                 }
+                 vetor[j] = key;
+             }
+             h = h / 2;
+         }
+         return vetor;
     }
 
     public static void printarVetor(int vetor[]) {
         System.out.print("vetor [ ");
         for(int i = 0; i < vetor.length; i++) {
-            System.out.print(vetor[i]+" ");
+            System.out.print(vetor[i] + " ");
         }
-        System.out.print("];");
-        System.out.println("");
+        System.out.print("]; ");
+        System.out.println(" ");
     }
+
 }
